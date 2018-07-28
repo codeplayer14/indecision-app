3ec6ab3bd300216176860bd782eb07db466ca49a
Padmanabh Gupta <padmanabh14@gmail.com>
2018-07-28 15:12:50 +0530

    Added normalized css

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -71,6 +71,7 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle} />
+       <div className='container'>
         <Action
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
@@ -86,6 +87,7 @@ export default class IndecisionApp extends React.Component {
         <OptionModal selectedOption = {this.state.selectedOption}
             handleClearSelectedOption={this.handleClearSelectedOption}
         />
+        </div>
       </div>
     );
   }
